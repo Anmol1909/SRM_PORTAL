cd66b947b402776dc951e8e404a23b1678563d21
Anmol Kumar <141660121+Anmol1909@users.noreply.github.com>
2026-07-04 00:47:45 +0530

    Update apiBase.js

--- SRM_PROJECT/src/utils/apiBase.js
+++ SRM_PROJECT/src/utils/apiBase.js
@@ -1,6 +1,6 @@
 export function getApiBaseUrl() {
   return (
     import.meta.env.VITE_API_BASE_URL ||
-    'http://127.0.0.1/SUPPLIER-RELATIONSHIP-MANAGEMENT-main/SRM_PROJECT/backend/api'
+    'https://srm-backend-zw9h.onrender.com/api'
   ).replace(/\/$/, '');
 }
